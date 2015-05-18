89af6139538b322b63d7e9767c6f082415cf5a56
tbehner <behner@cs.uni-bonn.de>
2015-05-18 17:18:55 +0200

    Added working DNS sniffer

--- practical/dns_sniffer.py
+++ practical/dns_sniffer.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 import pcap
 import dpkt
-
-def handler(ts, pkt, d):
-  udpPkt = dpkt.udp.UDP(pkt)
-  data = udpPkt.data
-
-  print "Received ", data, " at ", ts
+import socket
 
 pc = pcap.pcap()
 pc.setfilter('udp')
-pc.loop(handler)
 
+for ts, pkt in pc:
+  eth = dpkt.ethernet.Ethernet(pkt)
+  ip = eth.data
+  udp = ip.data
+  if udp.dport != 53 and udp.sport != 53:
+    continue
+
+  try:
+    dns = dpkt.dns.DNS(udp.data)
+    if udp.sport == 53:
+      for answer in dns.an:
+        if answer.type == dpkt.dns.DNS_A:
+          print 'Answer: ', answer.name, '\tIP Address: ', socket.inet_ntoa(answer.rdata)
+    elif udp.dport == 53:
+      print 'Query: ', dns.qd[0].name
+  except:
+    continue
 
